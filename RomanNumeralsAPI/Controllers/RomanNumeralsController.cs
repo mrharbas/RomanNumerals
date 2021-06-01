5fd68ebbb429c174996937d28ea17e33bf964be5
Jonatas Harbas <jonatas.harbas@gmail.com>
2021-06-01 18:40:08 -0300

    API

---
+++ RomanNumeralsAPI/Controllers/RomanNumeralsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RomanNumerals;
+using RomanNumeralsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RomanNumeralsController : ControllerBase
+    {
+        /// <summary>
+        /// Converts from or to Roman Numerals
+        /// </summary>
+        /// <returns>Returns the value converted</returns>
+        /// <response code="200">Returns the result parsed</response>
+        /// <response code="400">If conversion is impossible</response>
+        [HttpGet("[action]/{value}")]
+        public IActionResult Parse(string value)
+        {
+            Result result = new(value);
+
+            try
+            {
+                bool isNumber = int.TryParse(value, out int number);
+
+                result.ValueResult = isNumber ? RomanNumerals.RomanNumerals.Converts(number) : RomanNumerals.RomanNumerals.Converts(value);
+                result.Success = true;
+                result.Message = "Success.";
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                result.Success = false;
+                result.Message = e.Message;
+
+                return BadRequest(result);
+            }
+        }
+    }
+}
---
+++ RomanNumeralsAPI/Models/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsAPI.Models
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public object ValueToParse { get; set; }
+        public object ValueResult { get; set; }
+        public string Message { get; set; }
+
+        public Result(object valueToParse)
+        {
+            ValueToParse = valueToParse;
+        }
+    }
+}
